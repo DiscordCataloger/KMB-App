886b9b802f0b9aa2f307e8075becd039b36ac782
le,;wlc <jckjdkdmd@gmail.com>
2024-06-21 13:39:15 +0800

    initiation of js

---
+++ script.js
@@ -0,0 +1,28 @@
+const input = document.getElementsByTagName("input")[0];
+const processedInput = input.value
+  .split(/[\s-]+/)
+  .join()
+  .toUpperCase();
+input.value = processedInput;
+const output = document.getElementById("output");
+
+function processInput(data, input) {
+  const filter = data.filter((el) => el.route === input.value);
+  if (filter.length > 0) {
+    for (i = 0; i < filter.length; i++) {
+      const route = document.createElement("div");
+      route.className = "rounded-lg w-3";
+      route.innerText = `${data[i].origin_tc} → ${data[i].dest_tc}`;
+      output.appendChild(route);
+    }
+  } else {
+    const route = document.createElement("div");
+  }
+}
+
+async function fetchKMB() {
+  const res = await fetch(`https://data.etabus.gov.hk/v1/transport/kmb/route/`);
+  const json = await res.json();
+  const data = json.data;
+  processInput(data, input);
+}
